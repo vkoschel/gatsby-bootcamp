f9c3c0127436827a6b6bf26178349dad6e8049c8
Victor Koschel <victor.andrade@venturus.org.br>
2021-05-20 21:52:54 -0300

    Rendering post data in blog template

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,4 @@
+const { create } = require("domain")
 const path = require("path")
 
 module.exports.onCreateNode = ({ node, actions }) => {
@@ -12,4 +13,32 @@ module.exports.onCreateNode = ({ node, actions }) => {
       value: slug,
     })
   }
+}
+
+module.exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions
+    const blogTemplate = path.resolve('./src/templates/blog.js')
+    const res = await graphql(`
+    query {
+        allMarkdownRemark {
+            edges {
+                node {
+                    fields {
+                        slug
+                    }
+                }
+            }
+        }
+    }`)
+
+    res.data.allMarkdownRemark.edges.forEach((edge) => {
+        createPage({
+            component: blogTemplate,
+            path: `/blog/${edge.node.fields.slug}`,
+            context: {
+                slug: edge.node.fields.slug
+            }
+        })
+    })
+
 }
