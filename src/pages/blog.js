7471e501376a118f196f76d6a21d38b78d3e8845
Victor Koschel <victor.andrade@venturus.org.br>
2021-05-21 00:10:27 -0300

    Retrieving rich text body from Contentful blog post - NO IMGS YET

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,16 +7,17 @@ import * as blogStyles from './blog.module.scss'
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
     query {
-      allMarkdownRemark {
+      allContentfulBlogPost (
+        sort: {
+          fields: published,
+          order:DESC
+        }
+      ) {
         edges {
           node {
-            frontmatter {
-              title
-              date
-            }
-            fields {
-              slug
-            }
+            title
+            slug
+            published(formatString:"MMM Do, YYYY")
           }
         }
       }
@@ -28,12 +29,12 @@ const BlogPage = () => {
       <Layout>
         <h1>Blog</h1>
         <ol className={blogStyles.posts}>
-            {data.allMarkdownRemark.edges.map((edge) => {
+            {data.allContentfulBlogPost.edges.map((edge) => {
                 return (
                     <li className={blogStyles.post}>
-                      <Link to={`/blog/${edge.node.fields.slug}`}>
-                        <h2>{edge.node.frontmatter.title}</h2>
-                        <p>{edge.node.frontmatter.date}</p>
+                      <Link to={`/blog/${edge.node.slug}`}>
+                        <h2>{edge.node.title}</h2>
+                        <p>{edge.node.published}</p>
                         </Link>
                     </li>
                 )
