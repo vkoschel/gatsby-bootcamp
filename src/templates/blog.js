2665e2d2c0197c5a3d5bd05ab1de4133c01f5fd4
Victor Koschel <victor.andrade@venturus.org.br>
2021-05-21 15:03:39 -0300

    fetching images from contentful

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,27 +1,52 @@
 import React from "react"
 import { graphql } from "gatsby"
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { renderRichText } from "gatsby-source-contentful/rich-text"
 
 import Layout from "../components/layout"
 
 export const query = graphql`
-query($slug: String!){
-  contentfulBlogPost(slug: {eq: $slug}) {
-    title
-    published(formatString: "MMM Do, YYYY")
-    body {
-      raw
+  query($slug: String!) {
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      published(formatString: "MMMM Do, YYYY")
+      body {
+        raw
+        references {
+          ... on ContentfulAsset {
+            contentful_id
+            __typename
+          }
+          file {
+            url
+            fileName
+          }
+        }
+      }
     }
   }
-
-}`
+`
 
 const Blog = props => {
+  const options = {
+    renderNode: {
+      "embedded-asset-block": node => {
+        return (
+          <>
+            <img
+              src={node.data.target.file.url}
+              alt={node.data.target.file.fileName}
+            />
+          </>
+        )
+      },
+    },
+  }
   return (
     <Layout>
       <h1>{props.data.contentfulBlogPost.title}</h1>
       <p>{props.data.contentfulBlogPost.published}</p>
-      {documentToReactComponents(JSON.parse(props.data.contentfulBlogPost.body.raw))}
+      {renderRichText(props.data.contentfulBlogPost.body, options)}
     </Layout>
   )
 }
