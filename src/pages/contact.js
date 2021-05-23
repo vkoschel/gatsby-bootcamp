966ab872b2550ae1b7cfa1f0798cca0c2c7f9db5
vkoschel <victorkoschel@gmail.com>
2021-05-23 16:31:56 -0300

    Delete local posts, changes in website content

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,9 @@ const contact = () => {
              <Layout>
                  <Head title="Contact" />
             <h1>Contact Info</h1>
-            <p>Call me maybe? <a href="https://instagram.com/victorkoschel" target="_blank"> @victorkoschel</a></p>
+            <p>Call me maybe?: <a href="https://br.linkedin.com/in/victor-koschel-493b581b9" target="_blank">Linkedin</a></p>
+            <p>Projects: <a href="https://github.com/vkoschel" target="_blank"> @vkoschel</a></p>
+            <p>Follow me! <a href="https://instagram.com/victorkoschel" target="_blank"> @victorkoschel</a></p>
             </Layout>
         </div>
     )
--- src/posts/react.md
+++
@@ -1,6 +0,0 @@
----
-title: "React"
-date: "2021-19-05"
----
-
-In this post you'll learn React.
